9d3d4fb3ea9984aace8c83e5dd72ec50c2650f20
skyleira <tyler.b.hively@gmail.com>
2024-08-30 12:08:18 -0400

    Revised code to allow user input of millimeter rate of change.

--- Chapter2Ex7_Tyler_Hively.cpp
+++ Chapter2Ex7_Tyler_Hively.cpp
@@ -12,20 +12,30 @@ write a program that displays:
 • The number of millimeters higher than the current level that the ocean’s level will be in 7 years.
 
 • The number of millimeters higher than the current level that the ocean’s level will be in 10 years.
+
+Once you have coded this and placed in your main branch in GitHub, create a second branch that revises your code so
+
+that the user can enter the number of the number of millimeters the ocean level rises per year..
 */
 #include <iostream>
 using namespace std;
-const double levelRise = 1.5;
+//const double levelRise = 1.5;
 
 int main()
 {
-   
-	double YearLevel5 = levelRise * 5;
-	double YearLevel7 = levelRise * 7;
-	double YearLevel10 = levelRise * 10;
-	cout << "After 5 years, the ocean will have risen " << YearLevel5 << " millimeters" <<endl;
-	cout << "After 7 years, the ocean will have risen " << YearLevel7 << " millimeters" <<endl;
-	cout << "After 10 years, the ocean will have risen " << YearLevel10 << " millimeters" <<endl;
+	double levelRise;
+	double YearLevel5;
+	double YearLevel7;
+	double YearLevel10;
+	cout << "Please enter the millimeter rise you expect each year: ";
+	cin >> levelRise;
+	
+	YearLevel5 = levelRise * 5;
+	YearLevel7 = levelRise * 7;
+	YearLevel10 = levelRise * 10;
+	cout << "After 5 years, the ocean will have risen " << YearLevel5 << " millimeters" << endl;
+	cout << "After 7 years, the ocean will have risen " << YearLevel7 << " millimeters" << endl;
+	cout << "After 10 years, the ocean will have risen " << YearLevel10 << " millimeters" << endl;
 	return 0;
 }
 
